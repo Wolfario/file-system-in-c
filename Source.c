322f38ae728a108613b7b320193262bb3f3f3f91
Maksim Elizov <elizovmaksim1@gmail.com>
2021-07-16 21:28:03 +0200

    Begin of menu.
    Switch loop. Reaction to a, d and r letters.

--- Source.c
+++ Source.c
@@ -1,8 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int main()
 {
+	char str[80];
+	scanf("%79s", str);
+	char *token;
 
+	token = strtok(str, " ");
+
+	switch (token[0])
+	{
+	case 'a':
+		printf("%s", "Add Function");
+		break;
+	case 'r':
+		printf("%s", "Read Function");
+		break;
+	case 'd':
+		printf("%s", "Delete Function");
+		break;
+	default:
+		printf("%s", "Fatal Error");
+		break;
+	}
 
 	return 0;
 }
