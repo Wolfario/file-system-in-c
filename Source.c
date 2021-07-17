44e44c9bdbd8ef085023d924a0b235d87310baf2
Maksim Elizov <elizovmaksim1@gmail.com>
2021-07-17 13:35:02 +0200

    Big step
    menu, read function and bugs

--- Source.c
+++ Source.c
@@ -1,37 +1,118 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <locale.h>
+#include <string.h>
+#include <conio.h>
 
-int main()
+
+int program_status = 1;
+
+void read_data()
+{
+	FILE* fin;
+	int a = 0;
+
+	fin = fopen("storage.txt", "r");
+	fscanf(fin, "%d", &a);
+	printf("%d", a);
+	fclose(fin);
+}
+
+void help()
+{
+	system("cls");
+
+	printf("\033[0;31m");
+	printf("LOGO: \n");
+	printf("\033[0m");
+	printf("%s", "Command list:\n a - The command to add an item to the repository\n d - The command to delete an item from the repository\n r - The command for reading an item from the repository\n");
+}
+
+void menu()
 {
 	char str[80];
+	const char space_check[4] = " ";
 	scanf("%79s", str);
-	char *token;
 
-	token = strtok(str, " ");
+	char help_checker[5] = "help";
+	int help_checker_ind = strcmp(str, help_checker);
 
-	if (!token[1]) // Дописать принятие хелп функции
+	char exit_checker[5] = "exit";
+	int exit_checker_ind = strcmp(str, exit_checker);
+
+	if (!str[1] || help_checker_ind == 0 || exit_checker_ind == 0)
 	{
-		switch (token[0])
+		switch (str[0])
 		{
 		case 'a':
 			printf("%s", "Add Function");
 			break;
 		case 'r':
-			printf("%s", "Read Function");
+			system("cls");
+			read_data();
 			break;
 		case 'd':
 			printf("%s", "Delete Function");
 			break;
 		case 'h':
-			printf("%s", "Help Function");
-			break;
+			if (help_checker_ind == 0)
+			{
+				help();
+				break;
+			}
+			else
+			{
+				system("cls");
+				printf("\033[0;31m");
+				printf("LOGO: ");
+				printf("\033[0m");
+				printf("%s", "Fatal Error\n");
+				break;
+			}
+
+		case 'e':
+			if (exit_checker_ind == 0)
+			{
+				program_status = 0;
+				break;
+			}
+			else
+			{
+				system("cls");
+				printf("\033[0;31m");
+				printf("LOGO: ");
+				printf("\033[0m");
+				printf("%s", "*Fatal Error");
+				break;
+			}
 		default:
-			printf("%s", "Fatal Error");
+			system("cls");
+			printf("\033[0;31m");
+			printf("LOGO: ");
+			printf("\033[0m");
+			printf("%s", "Fatal Error\n");
 			break;
 		}
 	}
 	else
-		printf("%s", "Fatal Error");
+	{
+		system("cls");
+		printf("\033[0;31m");
+		printf("LOGO: ");
+		printf("\033[0m");
+		printf("%s", "Fatal Error\n");
+	}
+		
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	while (program_status == 1)
+	{
+		menu();
+	}
 	
 	return 0;
 }
