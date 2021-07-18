41c87b624d3189b06f0568fe1acc3cb3cc18f90e
Maksim Elizov <elizovmaksim1@gmail.com>
2021-07-18 14:36:40 +0200

    Almost final version of read function.

--- Source.c
+++ Source.c
@@ -7,25 +7,100 @@
 
 int program_status = 1;
 
+void print_logo()
+{
+	printf("\033[0;31m");
+	printf("LOGO: ");
+	printf("\033[0m");
+}
+
 void read_data()
 {
 	FILE* fin;
-	int a = 0;
+	int checker = 2;
+	char str[80];
+	char buffer[1024];
+	char* token;
+	const char determinant_list[3] = ";~";
+	const char determinant_side[2] = "~";
 
-	fin = fopen("storage.txt", "r");
-	fscanf(fin, "%d", &a);
-	printf("%d", a);
-	fclose(fin);
+	if ((fin = fopen("storage.txt", "r")) == NULL)
+	{
+		print_logo();
+		printf("Не удалось открыть файл\n");
+	}
+	else 
+	{
+		printf("\033[0;31m");
+		printf("%s", "List of items. You need to enter the name of one of the proposed items:\n");
+		printf("\033[0m");		
+
+		fin = fopen("storage.txt", "r");
+		fgets(buffer, 1023, fin);
+		
+		token = strtok(buffer, determinant_list);
+		while (token != NULL)
+		{
+			if ((checker % 2) == 0)
+			{
+				printf("%s\n", token);
+			}
+			
+			token = strtok(NULL, determinant_list);
+			checker++;
+		}
+		
+		fclose(fin);
+		if ((fin = fopen("storage.txt", "r")) == NULL)
+		{
+			print_logo();
+			printf("Не удалось открыть файл\n");
+		}
+		
+		checker = 2;
+		fgets(buffer, 1023, fin);
+		token = strtok(buffer, determinant_list);
+		scanf("%79s", str);
+
+		while (token != NULL && checker != 0)
+		{
+			if ((checker % 2) == 0)
+			{
+				if (strcmp(str, token) == 0)
+				{
+					system("cls");
+					printf("Name: %s\n", token);
+					token = strtok(NULL, determinant_list);
+					printf("Value: %s\n", token);
+					checker = -1;
+				}
+					
+			}
+
+			token = strtok(NULL, determinant_list);
+			++checker;
+		}
+
+		fclose(fin);
+
+		if (checker != 0)
+		{
+			system("cls");
+			printf("\033[0;31m");
+			printf("%s", "An invalid item name was entered. Try again:\n");
+			printf("\033[0m");
+			read_data();
+		}	
+	}
+
 }
 
 void help()
 {
 	system("cls");
 
-	printf("\033[0;31m");
-	printf("LOGO: \n");
-	printf("\033[0m");
-	printf("%s", "Command list:\n a - The command to add an item to the repository\n d - The command to delete an item from the repository\n r - The command for reading an item from the repository\n");
+	print_logo();
+	printf("%s", "\nCommand list:\n a - The command to add an item to the repository\n d - The command to delete an item from the repository\n r - The command for reading an item from the repository\n");
 }
 
 void menu()
@@ -63,9 +138,7 @@ void menu()
 			else
 			{
 				system("cls");
-				printf("\033[0;31m");
-				printf("LOGO: ");
-				printf("\033[0m");
+				print_logo();
 				printf("%s", "Fatal Error\n");
 				break;
 			}
@@ -79,17 +152,13 @@ void menu()
 			else
 			{
 				system("cls");
-				printf("\033[0;31m");
-				printf("LOGO: ");
-				printf("\033[0m");
+				print_logo();
 				printf("%s", "*Fatal Error");
 				break;
 			}
 		default:
 			system("cls");
-			printf("\033[0;31m");
-			printf("LOGO: ");
-			printf("\033[0m");
+			print_logo();
 			printf("%s", "Fatal Error\n");
 			break;
 		}
@@ -97,9 +166,7 @@ void menu()
 	else
 	{
 		system("cls");
-		printf("\033[0;31m");
-		printf("LOGO: ");
-		printf("\033[0m");
+		print_logo();
 		printf("%s", "Fatal Error\n");
 	}
 		
