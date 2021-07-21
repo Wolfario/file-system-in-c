dce756cb39b22d7a87a80d048eef2241550d2de4
Maksim Elizov <elizovmaksim1@gmail.com>
2021-07-21 19:54:03 +0200

    add almost entire add function.
    that needs more thoroughly processing

--- Source.c
+++ Source.c
@@ -14,6 +14,73 @@ void print_logo()
 	printf("\033[0m");
 }
 
+void add_data()
+{
+	FILE* fin;
+	int checker = 2;
+	char buffer[1024];
+	char* token;
+	const char determinant_list[3] = ";~";
+	char potentional_name[100];
+	char potentional_value[100];
+
+	if ((fin = fopen("storage.txt", "r")) == NULL)
+	{
+		print_logo();
+		printf("Не удалось открыть файл\n");
+	}
+	else
+	{
+		fgets(buffer, 1023, fin);
+
+		token = strtok(buffer, determinant_list);
+		printf("Please enter the name of your item: ");
+		scanf("%99s", potentional_name);
+		while (token != NULL && checker != 0)
+		{
+			if (checker % 2 == 0 && checker != -1)
+			{
+				if (strcmp(potentional_name, token) == 0)
+				{
+					system("cls");
+					print_logo();
+					printf("An item with this name already exists, please name it differently.\n");
+					checker = -1;
+				}
+			}
+
+			token = strtok(NULL, determinant_list);
+			checker++;
+		}
+
+		fclose(fin);
+
+		if (checker != 0)
+		{
+			if ((fin = fopen("storage.txt", "a+")) == NULL)
+			{
+				print_logo();
+				printf("Не удалось открыть файл\n");
+			}
+			else
+			{
+				printf("\n");
+				printf("Please enter the value of your item: ");
+				scanf("%99s", potentional_value);
+				printf("\n");
+				fprintf(fin, "%s~%s;", potentional_name, potentional_value);
+			}
+			
+			fclose(fin);
+			system("cls");
+		}
+		else
+		{
+			add_data();
+		}
+	}
+}
+
 void read_data()
 {
 	FILE* fin;
@@ -22,7 +89,6 @@ void read_data()
 	char buffer[1024];
 	char* token;
 	const char determinant_list[3] = ";~";
-	const char determinant_side[2] = "~";
 
 	if ((fin = fopen("storage.txt", "r")) == NULL)
 	{
@@ -35,9 +101,8 @@ void read_data()
 		printf("%s", "List of items. You need to enter the name of one of the proposed items:\n");
 		printf("\033[0m");		
 
-		fin = fopen("storage.txt", "r");
 		fgets(buffer, 1023, fin);
-		
+	
 		token = strtok(buffer, determinant_list);
 		while (token != NULL)
 		{
@@ -78,7 +143,7 @@ void read_data()
 			}
 
 			token = strtok(NULL, determinant_list);
-			++checker;
+			++checker;	
 		}
 
 		fclose(fin);
@@ -106,7 +171,6 @@ void help()
 void menu()
 {
 	char str[80];
-	const char space_check[4] = " ";
 	scanf("%79s", str);
 
 	char help_checker[5] = "help";
@@ -120,7 +184,7 @@ void menu()
 		switch (str[0])
 		{
 		case 'a':
-			printf("%s", "Add Function");
+			add_data();
 			break;
 		case 'r':
 			system("cls");
